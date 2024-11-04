ac9e5a508647daf8d1fd03a47e4bdda39e25c767
Kashyap Rathod <rathodkashyap08044@gmail.com>
2024-11-04 18:00:18 +0530

    Add change

--- resources/views/adminTheme/default.blade.php
+++ resources/views/adminTheme/default.blade.php
@@ -2,10 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <title>Estimate Management System - @yield('title')</title>
-    <meta content="" name="description">
-    <meta content="" name="keywords">
 
 
     <!-- Include other styles -->
