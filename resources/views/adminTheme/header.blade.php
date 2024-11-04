38b25e3614f74989432becef2960e078aaf21b0e
Kashyap Rathod <rathodkashyap08044@gmail.com>
2024-11-04 17:51:27 +0530

    Add Changes

--- resources/views/adminTheme/header.blade.php
+++ resources/views/adminTheme/header.blade.php
@@ -3,7 +3,7 @@
 
     <div class="d-flex align-items-center justify-content-between">
         <a href="index.html" class="logo d-flex align-items-center">
-            <img src="{{ asset('adminTheme/assets/img/logo.png') }}" alt="">
+            <img src="{{ asset('adminTheme/assets/img/ca.jpg') }}" alt="">
             <span class="d-none d-lg-block">KDA</span>
         </a>
         <i class="bi bi-list toggle-sidebar-btn"></i>
@@ -21,7 +21,7 @@
             <li class="nav-item dropdown pe-3">
 
                 <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-                    <img src="{{ asset('adminTheme/assets/img/profile-img.jpg') }}" alt="Profile" class="rounded-circle">
+                    <img src="{{ asset('adminTheme/assets/img/user22.jpg') }}" alt="Profile" class="rounded-circle">
                     <span class="d-none d-md-block dropdown-toggle ps-2">{{ auth()->user()->name }}</span>
                 </a><!-- End Profile Iamge Icon -->
 
